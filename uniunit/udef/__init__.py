8a00a747a6eb0573a676beaa87971fee1c35c359
WANG Longqi <iqgnol@gmail.com>
2014-11-30 13:15:10 +0800

    adjust init behavior

--- uniunit/udef/__init__.py
+++ uniunit/udef/__init__.py
@@ -9,10 +9,10 @@ Read README.md for more information.
 :Author: WANG Longqi <iqgnol@gmail.com>
 :Date: 2014-11-27
 """
-
-__version__ = '0.1'
-
-from uniunit import uniUnit, get_base_unit
-from unum.units import *
 import unum
-from .udef import *
+from unum.units import *
+kgf = unum.Unum.unit('kgf',9.8*N)
+inch = unum.Unum.unit('inch', 2.54 * cm )
+pound = unum.Unum.unit('pound', 0.45359237 * kg )
+
+
